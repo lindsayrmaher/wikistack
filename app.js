8ed915928409e4871c7f9380a4f82ac5802fced9
Lindsay Maher <lindsaymah3r@gmail.com>
2018-01-22 12:03:08 -0500

    half half way

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var morgan = require('morgan');
 var express = require('express');
 var app = express();
-var bodyparser = require('body-parser');
+var bodyParser = require('body-parser');
 var nunjucks = require('nunjucks');
 var routes = require('./routes');
+var models = require('./models');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -15,10 +16,17 @@ var env = nunjucks.configure('views', {noCache: true});
 app.set('view engine', 'html');
 app.engine('html', nunjucks.render);
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(__dirname + '/public'));
 
-app.use('/', require('./routes/index'));
+app.use('/', require('./routes'));
 
 app.get('/', function(req, res) {
-	res.render('index');
+	res.render('./views');
 })
+
+models.db.sync({force: true})
+.then(function() {
+	app.listen(3000, function() {
+		console.log("it's listening on port 3000!")
+	});
+}).catch(console.error);
--- models/index.js
+++ models/index.js
@@ -1,32 +1,51 @@
 var Sequelize = require('sequelize');
-var db = new Sequelize('postgres://localhost:5432/wikistack');
+var db = new Sequelize('postgres://localhost:5432/wikistack', {
+    logging: false
+});
 
 
 var Page = db.define('page', {
     title: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
     },
     urlTitle: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false, 
     },
     content: {
-        type: Sequelize.TEXT
+        type: Sequelize.TEXT,
+        allowNull: false
     },
     status: {
         type: Sequelize.ENUM('open', 'closed')
+    },
+    route: {
+    	type: Sequelize.VIRTUAL,
+    	get(){
+    		return '/wiki/' + this.getDataValue('urlTitle');	
+    	} 
+
     }
+   
 });
 
 var User = db.define('user', {
     name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
     },
     email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
+         validate: {
+            isEmail: true
+        }
     }
 });
 
 module.exports = {
   Page: Page,
-  User: User
+  User: User,
+  db: db
 };
